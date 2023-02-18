92af0d50bb5522b5194479e44724c079cff8f80c
riyash0109 <91386877+riyash0109@users.noreply.github.com>
2023-02-18 17:10:10 +0530

    Main

--- app/src/main/java/com/example/eas2/MainActivity.java
+++ app/src/main/java/com/example/eas2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.eas2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,17 +14,19 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView i1,i2,i3,i4,i5,i6;
     private TextView t1,t2,t3,t4,t5,t6;
+    private CardView cv1,cv2;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        i1 = (ImageView) findViewById(R.id.image1);
+        cv1 = (CardView) findViewById(R.id.china1);
 
         //calling the i1 function
-        i1.setOnClickListener(new View.OnClickListener() {
+        cv1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent it = new Intent(MainActivity.this, ChinaActivity.class);
@@ -30,8 +34,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        i2 = (ImageView) findViewById(R.id.image2);
-        i2.setOnClickListener(new View.OnClickListener() {
+        cv2 = (CardView) findViewById(R.id.japan1);
+        cv2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent it = new Intent(MainActivity.this, JapanActivity.class);
