75a315af38d47d7b80f0985cee0429abb3c26eeb
riyash0109 <91386877+riyash0109@users.noreply.github.com>
2023-03-07 15:08:43 +0530

    7thMarch2023

---
+++ app/src/main/java/com/example/eastasiastudiesapplication/CountryHome.java
@@ -0,0 +1,22 @@
+package com.example.eastasiastudiesapplication;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.zip.Inflater;
+
+public class CountryHome extends Fragment {
+
+    @NonNull
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_country_home, container, false);
+        return v;
+    }
+}
